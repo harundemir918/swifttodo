d0ddb3a729267a57dd8d44964628a22e5d797a7b
Harun Demir <harun.demir918@outlook.com>
2025-05-31 14:00:43 +0300

    Initial Commit

---
+++ swifttodo/swifttodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  swifttodoApp.swift
+//  swifttodo
+//
+//  Created by x3 on 31.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct swifttodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ swifttodoTests/swifttodoTests.swift
@@ -0,0 +1,17 @@
+//
+//  swifttodoTests.swift
+//  swifttodoTests
+//
+//  Created by x3 on 31.05.2025.
+//
+
+import Testing
+@testable import swifttodo
+
+struct swifttodoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
